9a6495f8c645368a968ee16799dba22e1eb4c071
Prateek Nepaliya <prateeknepalia9@gmail.com>
2016-04-15 02:46:00 +0530

    updated

--- Ex01.py
+++ Ex01.py
@@ -8,11 +8,11 @@ Created on Fri Apr 15 00:56:20 2016
 """Write a program that maps a list of words into a list 
 of integers representing the lengths of the correponding words."""
 
-def words_length(lst):
+def words_length_in_list(lst):
   lenlist = [len(i) for i in lst]
   return lenlist
 
 #test
-print words_length(['Hello', 'world!'])
-print words_length(['Python', 'is', 'awesome!'])
-print words_length(['You', 'said', 'it', 'bro!'])
+print words_length_in_list(['Hello', 'world!'])
+print words_length_in_list(['Python', 'is', 'awesome!'])
+print words_length_in_list(['You', 'said', 'it', 'bro!'])
