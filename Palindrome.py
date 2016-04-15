921bb085a3aa8a121bc73dc867e09847bf2e951d
Prateek Nepaliya <prateeknepalia9@gmail.com>
2016-04-15 22:44:41 +0530

    updated

--- Palindrome.py
+++ Palindrome.py
@@ -7,10 +7,7 @@ Created on Fri Apr 15 02:32:04 2016
 
 """Write a version of a palindrome recognizer that also accepts 
 phrase palindromes such as "Go hang a salami I'm a lasagna hog.", 
-"Was it a rat I saw?", "Step on no pets", "Sit on a potato pan, Otis", 
-"Lisa Bonet ate no basil", "Satan, oscillate my metallic sonatas", 
-"I roamed under it as a tired nude Maori", "Rise to vote sir", or 
-the exclamation "Dammit, I'm mad!". Note that punctuation, capitalization, 
+"Was it a rat I saw?" Note that punctuation, capitalization, 
 and spacing are usually ignored."""
 
 import string
@@ -27,11 +24,5 @@ def is_palindrome(str):
 #test
 print is_palindrome("Go hang a salami I'm a lasagna hog.")
 print is_palindrome("Was it a rat I saw?")
-print is_palindrome("Step on no pets")
-print is_palindrome("Sit on a potato pan, Otis!")
-print is_palindrome("Lisa Bonet ate no basil")
-print is_palindrome("Satan, oscillate my metallic sonatas")
-print is_palindrome("I roamed under it as a tired nude Maori")
-print is_palindrome("Rise to vote sir")
 print is_palindrome("Dammit, I'm mad!")
 print is_palindrome("This is not a palindrome")
