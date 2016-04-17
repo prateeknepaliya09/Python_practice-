ff9bc6302bfda49772210b9f0ec7242b8b93b093
Prateek Nepaliya <prateeknepalia9@gmail.com>
2016-04-17 23:25:44 +0530

    updated

---
+++ vowel_detection.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 17 23:25:11 2016
+
+@author: Prateek
+"""
+
+"""Write a function that takes a character (i.e. a 
+string of length 1) and returns True if it is a vowel, 
+False otherwise."""
+
+vowels = ['a', 'e', 'i', 'o', 'u']
+
+def vornot(str):
+  if str in vowels:
+    return True
+  else:
+    return False
+
+#test
+print vornot('e')
+print vornot('1')
+print vornot('z')
