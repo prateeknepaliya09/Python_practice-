8be33d044ed384bde5c593199ab43416a4e7339c
Prateek Nepaliya <prateeknepalia9@gmail.com>
2016-04-19 23:59:14 +0530

    updated

---
+++ tuples.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 19 23:58:50 2016
+
+@author: Prateek
+"""
+"""
+Question:
+Write a program to generate and print another tuple whose values are even numbers in the given tuple (1,2,3,4,5,6,7,8,9,10). 
+
+Hints:
+
+Use "for" to iterate the tuple
+Use tuple() to generate a tuple from a list.
+
+Solution"""
+tp=(1,2,3,4,5,6,7,8,9,10)
+li=list()
+for i in tp:
+	if tp[i]%2==0:
+		li.append(tp[i])
+
+tp2=tuple(li)
+print tp2
+
