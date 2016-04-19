95ef6aaf6d970726d2c89418ce245a9aef679cef
Prateek Nepaliya <prateeknepalia9@gmail.com>
2016-04-19 23:56:13 +0530

    updated

---
+++ square_any.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 19 23:55:34 2016
+
+@author: Prateek
+"""
+"""
+Question:
+    Write a method which can calculate square value of number
+
+Hints:
+    Using the ** operator
+
+Solution:"""
+
+def square(num):
+    return num ** 2
+
+print square(2)
+print square(3)
