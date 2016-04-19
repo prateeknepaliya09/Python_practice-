c91ba31de62012ebb9b7bd3d6d79e178586706db
Prateek Nepaliya <prateeknepalia9@gmail.com>
2016-04-20 00:40:17 +0530

    permutation in python

---
+++ permutation.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 20 00:33:29 2016
+
+@author: Prateek
+"""
+
+"""Please write a program which prints all permutations of [1,2,3]
+
+
+Hints:
+Use itertools.permutations() to get permutations of list.
+
+Solution:
+"""
+import itertools
+print list(itertools.permutations([1,2,3]))
