e7cc19f3fcbecc5f9071e55c73ddfbb73e76d2a2
Prateek Nepaliya <prateeknepalia9@gmail.com>
2016-04-19 23:56:52 +0530

    updated

---
+++ int_to_string.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 19 23:56:33 2016
+
+@author: Prateek
+"""
+
+"""Question:
+Define a function that can convert a integer into a string and print it in console.
+
+Hints:
+
+Use str() to convert a number to string.
+
+Solution"""
+def printValue(n):
+	print str(n)
+
+printValue(3)
+	
