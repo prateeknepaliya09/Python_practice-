66be3e45b20c12acab0556169d9fa7c25c57a06c
Prateek Nepaliya <prateeknepalia9@gmail.com>
2016-04-20 00:42:01 +0530

    reverse in python

---
+++ reverse.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 20 00:41:12 2016
+
+@author: Prateek
+"""
+
+"""Please write a program which accepts a string from console and print it in reverse order.
+
+Example:
+If the following string is given as input to the program:
+
+rise to vote sir
+
+Then, the output of the program should be:
+
+ris etov ot esir
+
+Hints:
+Use list[::-1] to iterate a list in a reverse order.
+
+Solution:
+"""
+
+s=raw_input()
+s = s[::-1]
+print s
