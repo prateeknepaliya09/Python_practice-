0930775f2a6647d07a8af8a76f9a4ed4a447788c
Prateek Nepaliya <prateeknepalia9@gmail.com>
2016-04-21 00:51:33 +0530

    Runtime error

---
+++ runtime_error.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 21 00:50:57 2016
+
+@author: Prateek
+"""
+
+"""
+Please raise a RuntimeError exception.
+
+Hints:
+
+Use raise() to raise an exception.
+
+Solution:"""
+
+raise RuntimeError('something wrong')
