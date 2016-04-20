a0c00fc01545b4c0106fd433f5704876422fea34
Prateek Nepaliya <prateeknepalia9@gmail.com>
2016-04-21 00:56:31 +0530

    updated

---
+++ random_float.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 21 00:56:05 2016
+
+@author: Prateek
+"""
+"""
+Question:
+
+Please generate a random float where the value is between 10 and 100 using Python math module.
+
+
+
+Hints:
+Use random.random() to generate a random float in [0,1].
+
+
+Solution:
+"""
+import random
+print random.random()*100
