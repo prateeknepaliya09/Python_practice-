755e7b59499999724584da419a1085e65f9fbf0c
Prateek Nepaliya <prateeknepalia9@gmail.com>
2016-04-22 01:18:23 +0530

    working with matrix

---
+++ adding_matrix.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 22 01:17:54 2016
+
+@author: Prateek
+"""
+
+# Program to add two matrices using nested loop
+
+X = [[12,7,3],
+    [4 ,5,6],
+    [7 ,8,9]]
+
+Y = [[5,8,1],
+    [6,7,3],
+    [4,5,9]]
+
+result = [[0,0,0],
+         [0,0,0],
+         [0,0,0]]
+
+# iterate through rows
+for i in range(len(X)):
+   # iterate through columns
+   for j in range(len(X[0])):
+       result[i][j] = X[i][j] + Y[i][j]
+
+for r in result:
+   print(r)
