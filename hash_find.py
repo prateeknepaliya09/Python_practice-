59ab11c5b57daefe82b193b26fb85f54848c5f2e
Prateek Nepaliya <prateeknepalia9@gmail.com>
2016-04-23 01:23:53 +0530

    hash find

---
+++ hash_find.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 23 01:23:34 2016
+
+@author: Prateek
+"""
+
+
+# Python rogram to find the SHA-1 message digest of a file
+
+# import hashlib module
+import hashlib
+
+def hash_file(filename):
+   """"This function returns the SHA-1 hash
+   of the file passed into it"""
+
+   # make a hash object
+   h = hashlib.sha1()
+
+   # open file for reading in binary mode
+   with open(filename,'rb') as file:
+
+       # loop till the end of the file
+       chunk = 0
+       while chunk != b'':
+           # read only 1024 bytes at a time
+           chunk = file.read(1024)
+           h.update(chunk)
+
+   # return the hex representation of digest
+   return h.hexdigest()
+
+message = hash_file("track1.mp3")
+print(message)
