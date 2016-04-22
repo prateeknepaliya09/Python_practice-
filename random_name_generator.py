12881ef470eb1b5b72f94ea5cac3e8e6d8046b20
Prateek Nepaliya <prateeknepalia9@gmail.com>
2016-04-23 01:36:37 +0530

    Generate random names through python

---
+++ random_name_generator.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 23 01:35:42 2016
+
+@author: Prateek
+"""
+
+#random name generator
+
+from random import randint
+
+
+def random_name_generator(first, second, x):
+    """
+        Generates random names.
+        Arguments:
+         - list of first names
+         - list of last names
+         - number of random names
+    """
+    names = []
+    for i in range(0, int(x)):
+        random_first = randint(0, len(first)-1)
+        random_last = randint(0, len(second)-1)
+        names.append("{0} {1}".format(
+            first[random_first],
+            second[random_last])
+        )
+    return set(names)
+
+
+first_names = ["Drew", "Mike", "Landon", "Jeremy", "Tyler", "Tom", "Avery"]
+last_names = ["Smith", "Jones", "Brighton", "Taylor"]
+names = random_name_generator(first_names, last_names, 5)
+print('\n'.join(names))
