2c4d8b42eb839b8c62903d88e6792f1d49c15849
Prateek Nepaliya <prateeknepalia9@gmail.com>
2016-04-26 21:46:59 +0530

    if else

---
+++ if_else.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 26 21:46:45 2016
+
+@author: Prateek
+"""
+
+var1 = 100
+if var1:
+   print "1 - Got a true expression value"
+   print var1
+else:
+   print "1 - Got a false expression value"
+   print var1
+
+var2 = 0
+if var2:
+   print "2 - Got a true expression value"
+   print var2
+else:
+   print "2 - Got a false expression value"
+   print var2
+
+print "Good bye!"
