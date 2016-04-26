e2a50ebe5eeb3ddba363133e0e33577a98d3cf0c
Prateek Nepaliya <prateeknepalia9@gmail.com>
2016-04-26 21:47:36 +0530

    if else2

---
+++ elif.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 26 21:47:14 2016
+
+@author: Prateek
+"""
+
+var = 100
+if var == 200:
+   print "1 - Got a true expression value"
+   print var
+elif var == 150:
+   print "2 - Got a true expression value"
+   print var
+elif var == 100:
+   print "3 - Got a true expression value"
+   print var
+else:
+   print "4 - Got a false expression value"
+   print var
+
+print "Good bye!"
