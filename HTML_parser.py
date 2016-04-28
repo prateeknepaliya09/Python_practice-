322dff31bf11613e76911234c45a5ac3676a892b
Prateek Nepaliya <prateeknepalia9@gmail.com>
2016-04-28 22:09:20 +0530

    HTML parser

---
+++ HTML_parser.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 28 22:09:04 2016
+
+@author: Prateek
+"""
+
+from html.parser import HTMLParser
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.strict = False
+        self.convert_charrefs= True
+        self.fed = []
+    def handle_data(self, d):
+        self.fed.append(d)
+    def get_data(self):
+        return ''.join(self.fed)
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
