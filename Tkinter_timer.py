645f2d3be12eaae9b627bba85cf430daf949ce9c
Prateek Nepaliya <prateeknepalia9@gmail.com>
2016-04-28 22:12:37 +0530

    Tkinter and python

---
+++ Tkinter_timer.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 28 22:12:21 2016
+
+@author: Prateek
+"""
+
+import Tkinter as tk
+import time
+
+class App():
+    def __init__(self):
+        self.root = tk.Tk()
+        self.label = tk.Label(text="")
+        self.label.pack()
+        self.update_clock()
+        self.root.mainloop()
+
+    def update_clock(self):
+        now = time.strftime("%H:%M:%S")
+        self.label.configure(text=now)
+        self.root.after(1000, self.update_clock)
+
+app=App()
